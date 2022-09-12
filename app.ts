f61522710503b145f2ed150e8ea5e139b9b3a7b0
yujiniii <youjin030700@gmail.com>
2022-09-12 15:06:41 +0900

    chore: init repository

---
+++ app.ts
@@ -0,0 +1,14 @@
+import  express, { Request, Response, NextFunction }  from "express";
+// import orderRoute from "";
+// import couponRoute from "";
+// import deliveryRoute from "";
+// import userRoute from "";
+
+const app:express.Application = express();
+
+app.listen(3000, ()=>{
+    console.log("server on....")
+})
+
+
+
